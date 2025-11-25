04d87d006f5f7bec7e096ad5f916b73edd117ce3
ThaisEvelen <thaisevelen@outlook.com>
2025-11-24 23:27:59 -0300

    exercicio 1

---
+++ modulo-01/exercicio01.py
@@ -0,0 +1,17 @@
+#Q1.
+#Faça um procedimento recursivo que receba dois valores inteiros a e b e 
+#imprime o intervalo fechado entre eles. Se a > b imprima uma mensagem de erro.
+
+a = input(int("Digite um valor:"))
+b = input(int("Digite um valor maior que o anterior:"))
+
+def intervalo (a, b):
+    if (a < b):
+        print("Valores invalidos")     
+    elif(a == b):
+        print("Caso base")
+        return a
+    else:
+        print(f'{b-a}')
+        return (b-a)
+print(f'A sequencia numérica é {b-a}')
